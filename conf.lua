6dac20919bc46c6a7e063e915f4e254070730039
Lynne <thecoolone3@gmail.com>
2018-08-11 17:19:56 +1000

    removed more unneeded modules

--- conf.lua
+++ conf.lua
@@ -6,11 +6,11 @@ function love.conf(t)
   t.modules.joystick = false
   t.modules.audio = true
   t.modules.keyboard = true
-  t.modules.event = true
+  t.modules.event = false
   t.modules.image = true
   t.modules.graphics = true
   t.modules.timer = true
-  t.modules.mouse = true
+  t.modules.mouse = false
   t.modules.sound = true
   t.modules.physics = false
 end
